82275d742c52a61499a88bc0b4cd4f4a1c821c38
Randolf <1665718426@qq.com>
2020-12-04 17:36:38 +0800

    📖 DOC: 📦 NEW: pytest+allure 生成测试报告

--- doc/paradigm/test_style.md
+++ doc/paradigm/test_style.md
@@ -6,3 +6,13 @@
 ### Reference
 
 1. [PyTest](https://www.jianshu.com/p/f3f13f2e6668)
+   1. [参考](https://blog.csdn.net/qq_42610167/article/details/101204066)
+2. [allure](https://docs.qameta.io/allure/#_pytest)
+   1. [生成html测试报告](https://www.jb51.net/article/183904.htm)
+      1. 注意，按照`test_sample.py`生成的报告存在一个问题，直接打开html显示文件一直loading，这是因为html加载js,css是通过相对链接，因此需要创建一个小的服务器打开，这样就行了。
+         1. [使用python创建服务器](https://developer.mozilla.org/zh-CN/docs/Learn/Common_questions/set_up_a_local_testing_server)
+         2. 因此主要命令：
+            1. cd 到test的路径/report/html
+            2. python -m http.server 
+            3. 此时命令行弹出一个本地链接，应该是http://0.0.0.0:8080/ , 直接用浏览器打开这个链接就ok
+            4. 目前看来这个生成的报告比较全面
--- test/test_sample.py
+++ test/test_sample.py
@@ -1,11 +1,56 @@
-def func(x):
-    """简单的测试代码
+ # ref:https://www.jb51.net/article/183904.htm
+ # ref:https://docs.qameta.io/allure/#_pytest
 
-    Args:
-        x (number): [测试的输入数]
-    """
-    return x+1
+import pytest
+import allure
 
+@allure.step('加法操作')
+def add(x):
+    return x + 2;
 
-def test_answer():
-    assert func(3) == 5
+@allure.step("步骤1:点xxx")
+def step_1():
+    print("111")
+
+@allure.step("步骤2:点xxx")
+def step_2():
+    print("222")
+
+@allure.feature('熟悉框架功能')# 用feature说明产品需求
+class TestClass(object):
+    @allure.story('测试加法') # 用story说明用户场景
+    def test_add(self):
+        assert add(2) == 4
+
+    @allure.story('测试包含') # 用story说明用户场景
+    def test_in(self):
+        a = 'hello world'
+        b = 'he'
+        step_1()
+        
+        assert b in a
+
+    @allure.story('测试不包含') # 用story说明用户场景
+    def test_not_in(self):
+        a = 'Hello'
+        b = 'hi'
+        step_2()
+        assert b not in a
+
+import os
+
+if __name__=="__main__":
+
+    #生成测试报告json
+
+    pytest.main(["-s", "-q", '--alluredir', 'report/result', 'test_sample.py'])
+
+    #将测试报告转为html格式
+
+    split='allure '+'generate '+'./report/result '+'-o '+'./report/html '+'--clean'
+
+    os.system('cd ./report')
+
+    os.system(split)
+
+    print(split)
