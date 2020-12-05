77f5bfa903291ff34289d353659071e2297bdf5d
Randolf <1665718426@qq.com>
2020-12-05 13:51:10 +0800

    📦 NEW: 完成抽象基类Pipe的定义

--- src/pipe/file_pipe.py
+++ src/pipe/file_pipe.py
@@ -4,6 +4,56 @@
 # Author       : randolf
 # Date         : 2020-12-03 15:46:57
 # LastEditors  : randolf
-# LastEditTime : 2020-12-03 15:46:57
+# LastEditTime : 2020-12-05 13:49:44
 # FilePath     : /CheeseBox/src/pipe/file_pipe.py
 # 
+
+import sys
+import os
+sys.path.append(os.getcwd())
+try:
+    sys.path.append(os.getcwd()+'/src')
+    sys.path.append(os.getcwd()+'/src/util')
+    sys.path.append(os.getcwd()+'/src/pipe')
+except Exception as e:
+    # 访问异常的错误编号和详细信息
+    print(e.args)
+    
+from file_tree import FileTree
+from file_node import FileNode
+from pipe import Pipe
+
+
+class FilePipe(Pipe):
+    
+    def __init__(self,file_system_destination, file_system_root_path):
+        """管道派生类，给定管道相关的存储位置(可以是本地的，也可以是网络的)
+        
+        FilePipe主要功能在于read & write, 对于本地的文件系统简单的进行读写操作。
+
+        Args:
+            file_system_destination (String): 管道出口(本地)的存储路径，可以用来备份文件树等等(write)
+            file_system_root_path (String): 管道入口(本地)的需要读操作的根目录，可以用来生成顶层文件树(read)
+        """
+        Pipe.__init__(self,file_system_destination,file_system_root_path)
+        
+    def read_file_system(self, read_file_system_relative_path=''):
+        """给定扫描文件系统的根目录，扫描出一个文件树
+
+        Args:
+            read_file_system_relative_path (String):文件系统目录，是一个相对于file_system_root_path的相对路径. Defaults to ''.
+
+        Returns:
+            FileTree: 生成的文件树
+        """
+        file_tree = None
+        return file_tree
+    
+    def write_file_system(self, file_system_name):
+        """根据系统中设定的file_system_destination往外写file_system的存储文件
+
+        Args:
+            file_system_name (String): 存储文件的名字
+        """
+        pass
+        
--- src/pipe/pipe.py
+++ src/pipe/pipe.py
@@ -4,7 +4,7 @@
 # Author       : randolf
 # Date         : 2020-12-03 15:46:35
 # LastEditors  : randolf
-# LastEditTime : 2020-12-05 00:35:07
+# LastEditTime : 2020-12-05 13:49:00
 # FilePath     : /CheeseBox/src/pipe/pipe.py
 # 
 
@@ -18,31 +18,50 @@ except Exception as e:
     # 访问异常的错误编号和详细信息
     print(e.args)
     
-from file_tree import FileTree
-from file_node import FileNode
+# 实现虚函数，ref: https://blog.csdn.net/tony_wong/article/details/39638887
+from abc import ABCMeta, abstractmethod
 
 
 class Pipe():
     """管道抽象类
-    #TODO: 仔细思考Pipe设计
-    """
     
-    def __init__(self, file_destination):
-        """管道基类，给定管道相关的存储位置(可以是本地的，也可以是网络的)
+    Pipe主要功能在于read & write, 而借助linux的思路，考虑虚拟文件系统的路子，屏蔽不同文件系统来源带来的差异(比如
+    网络和local的文件存储位置),简单的进行读写操作。
+
+    """
+    __metaclass__ = ABCMeta
+    
+    def __init__(self, file_system_destination, file_system_root_path):
+        """管道基类，抽象类，给定管道相关的存储位置(可以是本地的，也可以是网络的)
+        
+        Pipe主要功能在于read & write, 而借助linux的思路，考虑虚拟文件系统的路子，屏蔽不同文件系统来源带来的差异(比如
+        网络和local的文件存储位置),简单的进行读写操作。
 
         Args:
-            file_destination (String): 管道出口(本地/网络)的存储位置，可以用来备份文件树等等
+            file_system_destination (String): 管道出口(本地/网络)的存储路径，可以用来备份文件树等等(write)
+            file_system_root_path (String): 管道入口(本地/网络)的需要读操作的根目录，可以用来生成顶层文件树(read)
         """
-        self.file_destination = file_destination
-        
-    # def file_system_scan(self, file_system_root_path):
-    #     """给定扫描文件系统的根目录，扫描出一个文件树
+        self.file_system_destination = file_system_destination
+        self.file_system_root_path = file_system_root_path
+    
+    @abstractmethod
+    def read_file_system(self, read_file_system_relative_path=''):
+        """给定扫描文件系统的根目录，扫描出一个文件树
 
-    #     Args:
-    #         file_system_root_path (String): 文件系统根目录
+        Args:
+            read_file_system_relative_path (String):文件系统目录，是一个相对于file_system_root_path的相对路径. Defaults to ''.
 
-    #     Returns:
-    #         FileTree: [description]
-    #     """
-    #     file_tree = None
-    #     return file_tree
+        Returns:
+            FileTree: 生成的文件树
+        """
+        file_tree = None
+        return file_tree
+    
+    @abstractmethod
+    def write_file_system(self, file_system_name):
+        """根据系统中设定的file_system_destination往外写file_system的存储文件
+
+        Args:
+            file_system_name (String): 存储文件的名字
+        """
+        pass
